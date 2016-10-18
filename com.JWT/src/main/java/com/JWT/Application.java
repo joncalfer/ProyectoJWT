8d19cc46b727a61f79d387ef5b8da0344856391d
Jonathan <jonathan.calderon@centaurosolutions.com>
2016-10-18 16:44:43 -0600

    Simplificación de pom.xml y subida de archivo war al servidor

--- com.JWT/src/main/java/com/JWT/Application.java
+++ com.JWT/src/main/java/com/JWT/Application.java
@@ -1,13 +1,30 @@
 package com.JWT;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		
 	}
+	/*
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+
+    private static Class<Application> applicationClass = Application.class;
+    */
 }
+
+
